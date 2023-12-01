4966342ab1e110b486fcd3ac66cc69dfef2df94d
vil02 <vil02@o2.pl>
2023-12-01 15:37:05 +0100

    refactor: reduce code duplication

--- solutions/adv_2023_01.py
+++ solutions/adv_2023_01.py
@@ -3,7 +3,6 @@
 
 def parse_input(in_str):
     """parses the input into list of lines"""
-
     return in_str.splitlines()
 
 
@@ -11,49 +10,36 @@ def _get_sum(in_str):
     return int(in_str[0] + in_str[-1])
 
 
-def _get_num(in_line):
+def _extract_nums(in_str, in_nums):
     res = ""
-    for _ in in_line:
-        if _ in {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}:
-            res += _
-    return _get_sum(res)
+    for cur_pos in range(len(in_str)):
+        for _k, _v in in_nums.items():
+            if in_str[cur_pos:].startswith(_k):
+                res += _v
+                break
+    return res
+
+
+def _get_num(in_str, in_nums):
+    return _get_sum(_extract_nums(in_str, in_nums))
+
+
+_NUMS_A = {str(_): str(_) for _ in range(1, 10)}
 
 
 def solve_a(in_str):
     """returns the solution for part_a"""
-    return sum(_get_num(_) for _ in parse_input(in_str))
+    return sum(_get_num(_, _NUMS_A) for _ in parse_input(in_str))
 
 
-def _get_num_b(in_line):
-    nums = {
-        "1": "1",
-        "2": "2",
-        "3": "3",
-        "4": "4",
-        "5": "5",
-        "6": "6",
-        "7": "7",
-        "8": "8",
-        "9": "9",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-    res = ""
-    for cur_pos in range(len(in_line)):
-        for _k, _v in nums.items():
-            if in_line[cur_pos:].startswith(_k):
-                res += nums[_v]
-                break
-    return _get_sum(res)
+_NUMS_B = _NUMS_A | {
+    _k: str(_v)
+    for _v, _k in enumerate(
+        ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine"), 1
+    )
+}
 
 
 def solve_b(in_str):
     """returns the solution for part_b"""
-    return sum(_get_num_b(_) for _ in parse_input(in_str))
+    return sum(_get_num(_, _NUMS_B) for _ in parse_input(in_str))
