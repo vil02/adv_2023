8c7463eca3cfe6e341fae1cf6b528caaab4b7b31
Marysia ThinkPad <marysienka1992@o2.pl>
2023-12-03 18:55:34 +0100

    test: add test data for `adv_2023_01`

--- tests/test_adv_2023_01.py
+++ tests/test_adv_2023_01.py
@@ -3,10 +3,12 @@
 import test_utils as gu
 import solutions.adv_2023_01 as sol
 
-_INPUTS = gu.get_all_inputs(1, {"small", "small_2", "p"})
+_INPUTS = gu.get_all_inputs(1, {"small", "small_2", "p", "m"})
 
-test_solve_a_single = gu.get_test(sol.solve_a, {"small": 142, "p": 54667}, _INPUTS)
+test_solve_a_single = gu.get_test(
+    sol.solve_a, {"small": 142, "p": 54667, "m": 54388}, _INPUTS
+)
 
 test_solve_b_single = gu.get_test(
-    sol.solve_b, {"small": 142, "small_2": 281, "p": 54203}, _INPUTS
+    sol.solve_b, {"small": 142, "small_2": 281, "p": 54203, "m": 53515}, _INPUTS
 )
