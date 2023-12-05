7d7c6ba97b943871fc26062f6a2ffb33e35f690e
vil02 <vil02@o2.pl>
2023-12-05 16:26:43 +0100

    refactor: clenup `adv_2023_05`

--- solutions/adv_2023_05.py
+++ solutions/adv_2023_05.py
@@ -5,39 +5,42 @@ import collections
 Interval = collections.namedtuple("Interval", ["begin", "end"])
 
 
-def _is_not_empty(in_interval):
+def is_not_empty(in_interval):
+    """checks if the in_interval is not empty"""
     return in_interval.begin < in_interval.end
 
 
-class _MapRange:
+class IntervalShift:
+    """represents a mapping described by single interval"""
+
     def __init__(self, in_dest_start, in_source_start, in_len):
-        self.dest_start = in_dest_start
-        self.source_start = in_source_start
+        self._dest_start = in_dest_start
+        self._source_start = in_source_start
         assert in_len > 0
-        self.len = in_len
+        self._length = in_len
 
     def covers(self, in_val):
-        """checks if in_val is in this _MapRange"""
-        return self.source_start <= in_val <= self.source_start + self.len
+        """checks if in_val is in this interval"""
+        return self._source_start <= in_val <= self.last_source()
 
-    def map(self, in_val):
+    def __call__(self, in_val):
         """maps a single seed"""
         assert self.covers(in_val)
-        diff = in_val - self.source_start
-        return self.dest_start + diff
+        diff = in_val - self._source_start
+        return self._dest_start + diff
 
-    def source_end(self):
-        """returns the end of the interval"""
-        return self.source_start + self.len
+    def last_source(self):
+        """returns the last value, which is mapped by this mapping"""
+        return self._source_start + self._length
 
     def split(self, in_interval):
         """splits in_interval into tree part relative to this _MapRange"""
-        before = Interval(in_interval.begin, min(in_interval.end, self.source_start))
+        before = Interval(in_interval.begin, min(in_interval.end, self._source_start))
         mid = Interval(
-            max(in_interval.begin, self.source_start),
-            min(self.source_end(), in_interval.end),
+            max(in_interval.begin, self._source_start),
+            min(self.last_source(), in_interval.end),
         )
-        after = Interval(max(self.source_end(), in_interval.begin), in_interval.end)
+        after = Interval(max(self.last_source(), in_interval.begin), in_interval.end)
         return before, mid, after
 
 
@@ -45,29 +48,33 @@ class _Map:
     def __init__(self, in_domain, in_target, in_ranges):
         self.domain = in_domain
         self.target = in_target
-        self.ranges = in_ranges
+        self._shifts = in_ranges
 
-    def map(self, in_val):
-        """maps a single seed"""
-        for _ in self.ranges:
+    def __call__(self, in_val):
+        for _ in self._shifts:
             if _.covers(in_val):
-                return _.map(in_val)
+                return _(in_val)
         return in_val
 
+    def _map_single_interval(self, in_shift, in_interval):
+        before, mid, after = in_shift.split(in_interval)
+        final_res = []
+        if is_not_empty(mid):
+            final_res.append(Interval(in_shift(mid.begin), in_shift(mid.end)))
+
+        tmp_res = [_ for _ in [before, after] if is_not_empty(_)]
+        return final_res, tmp_res
+
     def map_intervals(self, in_intervals):
-        """maps intervals"""
+        """mapps the in_intervals using all of the stored IntervalShifts"""
         res = []
         tmp_intervas = in_intervals
-        for _ in self.ranges:
+        for cur_shift in self._shifts:
             mapped_intervals = []
-            while tmp_intervas:
-                before, mid, after = _.split(tmp_intervas.pop())
-                if _is_not_empty(before):
-                    mapped_intervals.append(before)
-                if _is_not_empty(mid):
-                    res.append(Interval(_.map(mid.begin), _.map(mid.end)))
-                if _is_not_empty(after):
-                    mapped_intervals.append(after)
+            for _ in tmp_intervas:
+                final_res, tmp_res = self._map_single_interval(cur_shift, _)
+                res.extend(final_res)
+                mapped_intervals.extend(tmp_res)
             tmp_intervas = mapped_intervals
         return res + tmp_intervas
 
@@ -92,7 +99,7 @@ def _parse_map(in_str):
     for line in lines[1:]:
         dest_start_str, source_start_str, len_str = line.split()
         ranges.append(
-            _MapRange(int(dest_start_str), int(source_start_str), int(len_str))
+            IntervalShift(int(dest_start_str), int(source_start_str), int(len_str))
         )
 
     return _Map(*_parse_domain_target(name), ranges)
@@ -112,7 +119,7 @@ def _parse_input(in_str):
 def _compute_final_location(in_seed, in_maps):
     cur = in_seed
     for _ in in_maps:
-        cur = _.map(cur)
+        cur = _(cur)
     return cur
 
 
@@ -123,11 +130,7 @@ def solve_a(in_str):
 
 
 def _seeds_to_intervals(in_seeds):
-    assert len(in_seeds) % 2 == 0
-    res = []
-    for _ in range(0, len(in_seeds), 2):
-        res.append(Interval(in_seeds[_], in_seeds[_] + in_seeds[_ + 1]))
-    return res
+    return [Interval(_s, _s + _l) for _s, _l in zip(in_seeds[0::2], in_seeds[1::2])]
 
 
 def _apply_all(maps, in_interval):
@@ -140,9 +143,8 @@ def _apply_all(maps, in_interval):
 def solve_b(in_str):
     """returns the solution for part_b"""
     seeds, maps = _parse_input(in_str)
-    intervals = _seeds_to_intervals(seeds)
     res_intervals = []
-    for _ in intervals:
+    for _ in _seeds_to_intervals(seeds):
         res_intervals.extend(_apply_all(maps, _))
 
     return min(_.begin for _ in res_intervals)
--- tests/test_adv_2023_05.py
+++ tests/test_adv_2023_05.py
@@ -1,7 +1,80 @@
 """tests of adv_2023_05"""
+import pytest
 
 import test_utils as gu
 import solutions.adv_2023_05 as sol
+
+
+@pytest.mark.parametrize(
+    "in_interval",
+    [sol.Interval(1, 2), sol.Interval(0, 10)],
+)
+def test_is_not_empty_positive(in_interval):
+    """positive tests of is_not_empty"""
+    assert sol.is_not_empty(in_interval)
+
+
+@pytest.mark.parametrize(
+    "in_interval",
+    [sol.Interval(0, 0), sol.Interval(2, 1)],
+)
+def test_is_not_empty_negative(in_interval):
+    """negative tests of is_not_empty"""
+    assert not sol.is_not_empty(in_interval)
+
+
+_INTERVAL_SHIFT = sol.IntervalShift(5, 10, 4)
+
+
+@pytest.mark.parametrize(
+    "in_seed, expected",
+    [(10, 5), (11, 6), (12, 7), (13, 8), (14, 9)],
+)
+def test_interval_shif(in_seed, expected):
+    """checks if IntervalShift mapps properly"""
+    assert _INTERVAL_SHIFT(in_seed) == expected
+
+
+@pytest.mark.parametrize(
+    "in_seed",
+    [9, 15],
+)
+def test_interval_shif_raises_error(in_seed):
+    """checks if IntervalShift if the input is outside the interval"""
+    with pytest.raises(Exception):
+        _INTERVAL_SHIFT(in_seed)
+
+
+@pytest.mark.parametrize(
+    "in_interval, expected",
+    [
+        (
+            sol.Interval(10, 14),
+            (sol.Interval(10, 10), sol.Interval(10, 14), sol.Interval(14, 14)),
+        ),
+        (
+            sol.Interval(9, 12),
+            (sol.Interval(9, 10), sol.Interval(10, 12), sol.Interval(14, 12)),
+        ),
+        (
+            sol.Interval(11, 12),
+            (sol.Interval(11, 10), sol.Interval(11, 12), sol.Interval(14, 12)),
+        ),
+        (
+            sol.Interval(13, 15),
+            (sol.Interval(13, 10), sol.Interval(13, 14), sol.Interval(14, 15)),
+        ),
+    ],
+)
+def test_interval_shif_split(in_interval, expected):
+    """checks IntervalShift.split"""
+    assert _INTERVAL_SHIFT.split(in_interval) == expected
+
+
+def test_interval_shif_last_source():
+    """tests the last_source method of IntervalShift"""
+    assert _INTERVAL_SHIFT.last_source() == 14
+
 
 _INPUTS = gu.get_all_inputs(5, {"small", "p"})
 
