2be0757b1f16b35d38556c9508670bf09738fcc6
vil02 <vil02@o2.pl>
2023-12-09 11:35:52 +0100

    style: use proper test names

--- tests/test_adv_2023_08.py
+++ tests/test_adv_2023_08.py
@@ -6,8 +6,6 @@ import solutions.adv_2023_08 as sol
 
 _INPUTS = tu.get_inputs(8, {"small", "small_a", "small_b", "p"})
 
-test_solve_a_single = _INPUTS.get_test(
-    sol.solve_a, {"small": 2, "small_a": 6, "p": 16271}
-)
+test_solve_a = _INPUTS.get_test(sol.solve_a, {"small": 2, "small_a": 6, "p": 16271})
 
-test_solve_b_single = _INPUTS.get_test(sol.solve_b, {"small_b": 6, "p": 14265111103729})
+test_solve_b = _INPUTS.get_test(sol.solve_b, {"small_b": 6, "p": 14265111103729})
