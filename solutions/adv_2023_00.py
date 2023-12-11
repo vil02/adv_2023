19b4fb6944d9f969ae7bf1253cb92c3cb8fc600e
PoJu Chen <pojuchen@uchicago.edu>
2023-12-10 19:45:00 -0600

    Add type annotation for solution of day 0

--- solutions/adv_2023_00.py
+++ solutions/adv_2023_00.py
@@ -1,22 +1,22 @@
 """solution of adv_2023_00"""
 
 
-def parse_input(in_str):
+def parse_input(in_str: str) -> list[int]:
     """parses the input into..."""
 
-    def _proc_single_line(in_line):
+    def _proc_single_line(in_line: str) -> int:
         return len(in_line)
 
     return [_proc_single_line(_) for _ in in_str.splitlines()]
 
 
-def solve_a(in_str):
+def solve_a(in_str: str) -> int:
     """returns the solution for part_a"""
     data = parse_input(in_str)
     return sum(data)
 
 
-def solve_b(in_str):
+def solve_b(in_str: str) -> int:
     """returns the solution for part_b"""
     data = parse_input(in_str)
     return 2 * sum(data)
