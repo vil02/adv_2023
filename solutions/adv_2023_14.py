c8f744a91d824d963dd285c864a2a1dbadb51208
vil02 <vil02@o2.pl>
2023-12-14 16:28:46 +0100

    refactor: cleanup `adv_2023_14`

--- solutions/adv_2023_14.py
+++ solutions/adv_2023_14.py
@@ -5,6 +5,10 @@ def _to_pos(in_x: int, in_y: int) -> tuple[int, int]:
     return (in_x, in_y)
 
 
+_ROUND_ROCK = "O"
+_EMPTY = "."
+
+
 def parse_input(in_str: str):
     """parses the input into a dict"""
     lines = in_str.splitlines()
@@ -22,7 +26,7 @@ def parse_input(in_str: str):
 def find_empty_north(in_image, y_size, x_pos, y_pos):
     """find the final y-position of the round rock while moving to the north"""
     cur_y_pos = y_pos + 1
-    while cur_y_pos < y_size + 1 and in_image[_to_pos(x_pos, cur_y_pos)] == ".":
+    while cur_y_pos < y_size + 1 and in_image[_to_pos(x_pos, cur_y_pos)] == _EMPTY:
         cur_y_pos += 1
     return cur_y_pos - 1
 
@@ -40,10 +44,10 @@ def _to_string(in_image, x_size, y_size):
 def _tilt_north(image, x_size, y_size):
     for y_pos in range(y_size, 0, -1):
         for x_pos in range(1, x_size + 1):
-            if image[_to_pos(x_pos, y_pos)] == "O":
+            if image[_to_pos(x_pos, y_pos)] == _ROUND_ROCK:
                 new_y_pos = find_empty_north(image, y_size, x_pos, y_pos)
-                image[_to_pos(x_pos, y_pos)] = "."
-                image[_to_pos(x_pos, new_y_pos)] = "O"
+                image[_to_pos(x_pos, y_pos)] = _EMPTY
+                image[_to_pos(x_pos, new_y_pos)] = _ROUND_ROCK
     return image
 
 
@@ -57,11 +61,7 @@ def _rotate_90(image, x_size, y_size):
 
 
 def _compute_load_score(in_image):
-    res = 0
-    for pos, char in in_image.items():
-        if char == "O":
-            res += pos[1]
-    return res
+    return sum(pos[1] for pos, char in in_image.items() if char == _ROUND_ROCK)
 
 
 def solve_a(in_str: str):
@@ -107,24 +107,26 @@ def _make_cycle(image, x_size, y_size):
 
 
 def _compute_cycle_data(image, x_size, y_size):
-    res = {}
+    cycle_data = {}
     changes = {}
     move_num = 0
-    while _to_string(image, x_size, y_size) not in res:
-        res[_to_string(image, x_size, y_size)] = move_num
+    while _to_string(image, x_size, y_size) not in cycle_data:
+        cycle_data[_to_string(image, x_size, y_size)] = move_num
         image = _make_cycle(image, x_size, y_size)
         move_num += 1
         changes[move_num - 1] = move_num
-    changes[move_num - 1] = res[_to_string(image, x_size, y_size)]
-    return res, changes
+    changes[move_num - 1] = cycle_data[_to_string(image, x_size, y_size)]
+
+    cycle_data = {_v: _k for _k, _v in cycle_data.items()}
+    return cycle_data, changes
 
 
 def _compute_cycle_info(in_changes):
     max_key = max(in_changes.keys())
     first_of_cycle = in_changes[max_key]
-    cycle_len = max_key - first_of_cycle
+    cycle_len = max_key - first_of_cycle + 1
 
-    return first_of_cycle, cycle_len + 1
+    return first_of_cycle, cycle_len
 
 
 def _compute_state(in_cycle_data, in_changes, in_cycle_num):
@@ -132,9 +134,7 @@ def _compute_state(in_cycle_data, in_changes, in_cycle_num):
     r_val = (in_cycle_num - first_of_cycle) % cycle_len
     reduced_move = r_val + first_of_cycle
 
-    after_move = {_v: _k for _k, _v in in_cycle_data.items()}
-
-    return after_move[reduced_move]
+    return in_cycle_data[reduced_move]
 
 
 def solve_b(in_str: str):
