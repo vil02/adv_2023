b2ba9891f233cd5037b71ac08a99a6a95deb6d36
vil02 <vil02@o2.pl>
2024-02-08 19:07:38 +0100

    style: add type hints to `adv_2023_04`

--- solutions/adv_2023_04.py
+++ solutions/adv_2023_04.py
@@ -5,20 +5,17 @@ import collections
 Card = collections.namedtuple("Card", ["id", "winning", "have"])
 
 
-def _parse_card_id_str(in_str):
+def _parse_card_id_str(in_str: str) -> int:
     _, id_str = in_str.split()
     assert _ == "Card"
     return int(id_str)
 
 
-def _parse_numbers_str(in_str):
-    res = set()
-    for _ in in_str.split():
-        res.add(int(_))
-    return res
+def _parse_numbers_str(in_str: str) -> set[int]:
+    return {int(_) for _ in in_str.split()}
 
 
-def _parse_card(in_str):
+def _parse_card(in_str: str) -> Card:
     card_str, all_nums = in_str.split(": ")
     winning_str, have_str = all_nums.split(" | ")
 
@@ -29,27 +26,27 @@ def _parse_card(in_str):
     )
 
 
-def _parse_input(in_str):
+def _parse_input(in_str: str) -> list[Card]:
     return [_parse_card(_) for _ in in_str.splitlines()]
 
 
-def _get_number_of_common(in_card):
+def _get_number_of_common(in_card: Card) -> int:
     return len(in_card.winning.intersection(in_card.have))
 
 
-def _count_points(in_card):
+def _count_points(in_card: Card) -> int:
     common_len = _get_number_of_common(in_card)
     if common_len == 0:
         return 0
     return 2 ** (common_len - 1)
 
 
-def solve_a(in_str):
+def solve_a(in_str: str) -> int:
     """returns the solution for part_a"""
     return sum(_count_points(_) for _ in _parse_input(in_str))
 
 
-def _evaluate_stack(in_cards):
+def _evaluate_stack(in_cards: list[Card]) -> int:
     hist = {_.id: 1 for _ in in_cards}
     total = 0
     for cur_card in in_cards:
@@ -64,6 +61,6 @@ def _evaluate_stack(in_cards):
     return total
 
 
-def solve_b(in_str):
+def solve_b(in_str: str) -> int:
     """returns the solution for part_b"""
     return _evaluate_stack(_parse_input(in_str))
